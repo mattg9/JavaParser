3e38d76116f0e23433a08211b122af71235b43e4
Matthew Gaston <matthew.gaston@broadcom.com>
2024-04-07 09:44:13 -0400

    docuemnt the abstract interface

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,6 +1,16 @@
 package parser;
 import model.RecordTable;
 
+/**
+ * This interface defines the operations for a generic data parser.
+ * Implementing classes should provide functionality to parse data into a RecordTable.
+ */
 public interface Parser {
+    
+    /**
+     * Parses data and populates the provided RecordTable with records.
+     * 
+     * @param table the RecordTable object to populate with records
+     */
     void parse(RecordTable table);
 }
